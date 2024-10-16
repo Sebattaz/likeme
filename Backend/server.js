69fdefbd649f7528de85a77da86a7cdf81ddbe7f
Sebattaz <seba.lostar@gmail.com>
2024-10-15 22:21:28 -0300

    instalacion y uso de cors

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,20 @@
 const express = require("express");
-const { getData,postData } = require("./consultas");
+const { getData, postData } = require("./consultas");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
-
+app.use(cors());
 app.listen(3000, () => console.log("Servidor Levantado"));
 
-app.get("/", async(req, res) =>{
-    const  likes = await getData();
+app.get("/posts", async (req, res) => {
+  const likes = await getData();
 
-    res.json(likes);
-})
+  res.json(likes);
+});
 
-app.post("/", async (req, res)=>{
-    const {titulo, img, descripcion, likes} = req.body;
-    await postData(titulo, img, descripcion, likes);
-    res.send("LIKE AGREGADO");
+app.post("/", async (req, res) => {
+  const { titulo, img, descripcion, likes } = req.body;
+  await postData(titulo, img, descripcion, likes);
+  res.send("LIKE AGREGADO");
 });
